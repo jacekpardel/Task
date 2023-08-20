cc82e56a307de755355181a57f1ddf3da3125c46
Jacek Pardel <jacekpardel@gmail.com>
2023-08-20 11:08:14 +0200

    add /creating a new lecturer/ function

--- Task/Controllers/LecturersController.cs
+++ Task/Controllers/LecturersController.cs
@@ -1,10 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IS_Task.Data;
+using Microsoft.AspNetCore.Mvc;
+using Task.Models;
 
 namespace IS_Task.Controllers
 {
     public class LecturersController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public LecturersController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
+        {
+            List <LecturerModel> objLecturersList  = _db.Lecturers.ToList();
+            return View(objLecturersList);
+        }
+               public IActionResult Create()
         {
             return View();
         }
